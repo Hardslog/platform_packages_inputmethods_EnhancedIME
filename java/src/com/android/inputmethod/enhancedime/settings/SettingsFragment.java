CyanogenMod/android_packages_inputmethods_LatinIME

LatinIME: Remove gesture preference if gesture lib is unavailable
Gesture typing won't work without the gesture library,
so remove the preference to not mislead users
into thinking that gesture typing is supported.

Change-Id: I387ae83fd174b57fc48fe7bcfc37e3010ce89b12

java/src/com/android/inputmethod/latin/settings/SettingsFragment.java
@@ -31,6 +31,7 @@ import com.android.inputmethod.latin.R;
 import com.android.inputmethod.latin.define.ProductionFlags;
 import com.android.inputmethod.latin.utils.ApplicationUtils;
 import com.android.inputmethod.latin.utils.FeedbackUtils;
+import com.android.inputmethod.latin.utils.JniUtils;
 import com.android.inputmethodcommon.InputMethodSettingsFragment;
 
 public final class SettingsFragment extends InputMethodSettingsFragment {
@@ -54,6 +55,10 @@ public final class SettingsFragment extends InputMethodSettingsFragment {
         if (!ProductionFlags.ENABLE_ACCOUNT_SIGN_IN) {
             final Preference accountsPreference = findPreference(Settings.SCREEN_ACCOUNTS);
             preferenceScreen.removePreference(accountsPreference);
+        }
+        if (!JniUtils.mHaveGestureLib) {
+            final Preference gesturePreference = findPreference(Settings.SCREEN_GESTURE);
+            preferenceScreen.removePreference(gesturePreference);
         }
     }
 

Hardslog/platform_packages_inputmethods_EnhancedIME

java/src/com/android/inputmethod/enhancedime/settings/SettingsFragment.java
@@ -31,6 +31,7 @@ import com.android.inputmethod.enhancedime.R;
 import com.android.inputmethod.latin.define.ProductionFlags;
 import com.android.inputmethod.enhancedime.utils.ApplicationUtils;
 import com.android.inputmethod.enhancedime.utils.FeedbackUtils;
+import com.android.inputmethod.enhancedime.utils.JniUtils;
 import com.android.inputmethodcommon.InputMethodSettingsFragment;
 
 public final class SettingsFragment extends InputMethodSettingsFragment {
@@ -54,6 +55,10 @@ public final class SettingsFragment extends InputMethodSettingsFragment {
         if (!ProductionFlags.ENABLE_ACCOUNT_SIGN_IN) {
             final Preference accountsPreference = findPreference(Settings.SCREEN_ACCOUNTS);
             preferenceScreen.removePreference(accountsPreference);
+        }
+        if (!JniUtils.mHaveGestureLib) {
+            final Preference gesturePreference = findPreference(Settings.SCREEN_GESTURE);
+            preferenceScreen.removePreference(gesturePreference);
         }
     }
 